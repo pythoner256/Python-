d9837d7350a0ba8cdc0ae042f084fe786f304075
pythoner256 <38498069+pythoner256@users.noreply.github.com>
2018-07-13 16:25:37 +0800

    更新文件

--- 冒泡排序.py
+++ 冒泡排序.py
@@ -1,4 +1,5 @@
 def bubble(n):
+    """从第一个元素开始依次和列表的元素进行比较，选出最大元素放到末尾"""
     for j in range(1, len(n)-1):
         for i in range(len(n)-j):
             if n[i] > n[i+1]:
@@ -9,3 +10,7 @@ def bubble(n):
 if __name__ == "__main__":
     n = eval(input("请输入一个数组: "))
     bubble(n)
+"""
+最优时间复杂度O(n)
+最坏时间复杂度O(n平方)
+"""
