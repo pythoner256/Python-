040f947a4abf4106914b3955171a1ae12f535fe9
pythoner256 <38498069+pythoner256@users.noreply.github.com>
2018-07-25 21:32:13 +0800

    上传文件

---
+++ sys 模块.py
@@ -0,0 +1,27 @@
+"""一些sys命令"""
+
+# sys.argv 命令行参数List，返回的是一个列表，第一个元素是程序本身路径
+#
+# sys.version 获取Python解释程序的版本信息
+#
+# sys.maxunicode 最大的Unicode值
+#
+# sys.modules 返回系统导入的模块字段，key是模块名，value是模块
+#
+# sys.path 返回模块的搜索路径，初始化时使用PYTHONPATH环境变量的值
+#
+# sys.platform 返回操作系统平台名称
+#
+# sys.stdout 标准输出
+#
+# sys.stdin 标准输入
+#
+# sys.stderr 错误输出
+#
+# sys.exc_clear() 用来清除当前线程所出现的当前的或最近的错误信息
+#
+# sys.exec_prefix 返回平台独立的python文件安装的位置
+#
+# sys.copyright 记录python版权相关的东西
+#
+# sys.api_version 解释器的C的API版本
